b29d12f0983e404c51435d4d897407f7a19b3c93
Kyunghoon Jo <10jobss@gmail.com>
2017-11-23 17:07:56 +0900

    Add files via upload

--- server.R
+++ server.R
@@ -1,9 +1,8 @@
 library(shiny)
-game <- read.csv("D:/Data_Set/google_game_all.csv", header=T)
-
-
+source("game_recommender.R")
 shinyServer(function(input, output) {
- # output$table <- renderTable({
-#    input$select1$label
-#  })
+   observeEvent(input$button, {
+    output$table <- renderTable({
+      game_recommender(input$select1, input$select2, input$select3, input$select4, input$select5)})
+   })
 })
