9066139dde5cf467871c9e7dcadc17538b8f3133
Kyunghoon Jo <10jobss@gmail.com>
2017-11-27 15:48:01 +0900

    Delete server.R

--- server.R
+++
@@ -1,8 +0,0 @@
-library(shiny)
-source("game_recommender.R")
-shinyServer(function(input, output) {
-   observeEvent(input$button, {
-    output$table <- renderTable({
-      game_recommender(input$select1, input$select2, input$select3, input$select4, input$select5)})
-   })
-})
